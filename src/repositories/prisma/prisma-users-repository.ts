b61906fd9d9cde81cf754c2e072c8fb8ccfd7754
carlosvalter <carlosvalter@hotmail.com>
2024-08-09 14:46:57 +0100

    Create interface User repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,7 +1,18 @@
 import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
 
-export class PrismaUserRepository {
+export class PrismaUserRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
   // data: o Prisma ja cria automaticamente varias tipagens para cada model
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
